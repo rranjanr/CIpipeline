17c6333e2587d5647698c9391fd06c891dfc4432
Rahul Ranjan <RAHUL1.777402@mmamc.tu.edu.np>
2025-05-06 12:57:18 +0545

    new

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ app=Flask(__name__)
 def webout():
 
  return '<h1>DevOps is so much fun to learn.</h1>'
+ return '<h1>Myself rahul Ranjan</h1>'
+ return '<h1>How are you</h1>'
 
 app.run(host='0.0.0.0',port=7000)
